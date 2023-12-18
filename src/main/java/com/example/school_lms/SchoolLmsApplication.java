33ab28097f07eefd8d8be36bdf1a3f2bd4c5a6cf
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-12-18 09:24:52 +0900

    초기설정

---
+++ src/main/java/com/example/school_lms/SchoolLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.school_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/school_lms/SchoolLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
