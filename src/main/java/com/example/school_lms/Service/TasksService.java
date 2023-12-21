13258b9831773b55ac45e499a6e28b3e9760563c
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-12-21 17:19:01 +0900

    과제 html / Controller 진행

---
+++ src/main/java/com/example/school_lms/Service/TasksService.java
@@ -0,0 +1,11 @@
+package com.example.school_lms.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TasksService {
+}
