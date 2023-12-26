e92cb35448131ba04586d4adf12e95bc5cb00b91
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-12-26 17:37:41 +0900

    homework

--- src/main/java/com/example/school_lms/repository/LectureRepository.java
+++ src/main/java/com/example/school_lms/repository/LectureRepository.java
@@ -1,9 +1,26 @@
 package com.example.school_lms.repository;
 
+
 import com.example.school_lms.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
+import java.util.ArrayList;
 
+public interface LectureRepository extends JpaRepository<Lecture, String> {
+
+    @Override
+    ArrayList<Lecture> findAll();
+
+    Lecture findBySubjectId(String subject_id);
+
+
+//    Lecture findById();
+
+//    Lecture findById(String subject_id);
+
+
+    // 검색기능
+//    List<Posts> findBySubjectNameContaining(String keyword);
+
+//    ArrayList<Lecture> findAllSubjectId(Long subject_id);
 }
-
