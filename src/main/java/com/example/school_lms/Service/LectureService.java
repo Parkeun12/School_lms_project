2db3b33ccb0f5f557780198f9d82f14e52e0db68
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-12-30 22:37:57 +0900

    2차 merge - nam

--- src/main/java/com/example/school_lms/Service/LectureService.java
+++ src/main/java/com/example/school_lms/Service/LectureService.java
@@ -1,115 +1,85 @@
-//package com.example.school_lms.Service;
-//
-//import com.example.school_lms.dto.LectureDto;
-//import com.example.school_lms.entity.Lecture;
-//import com.example.school_lms.entity.User;
-//import com.example.school_lms.entity.Userdata;
-//import com.example.school_lms.repository.LectureRepository;
-//import com.example.school_lms.repository.UserRepository;
-//import com.example.school_lms.repository.UserdataRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//public class LectureService {
-//
-//    @Autowired
-//    LectureRepository lectureRepository;
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    public static Lecture createLecture(LectureDto lectureDto) {
-//
-//        Lecture lecture = new Lecture();
-//
-//        lecture.setSubjectName(lectureDto.getSubjectName());
-//        lecture.setSubjectScore(lectureDto.getSubjectScore());
-//        lecture.setSubjectTime(lectureDto.getSubjectTime());
-//        lecture.setSubjectClassification(lectureDto.getSubjectClassification());
-//        lecture.setSubjectGrade(lectureDto.getSubjectGrade());
-//        lecture.setSubjectYear(lectureDto.getSubjectYear());
-////        lecture.setSubjectSemester(lectureDto.getSubjectSemester());
-//        lecture.setSubjectOutline(lectureDto.getSubjectOutline());
-//        lecture.setSubjectGoal(lectureDto.getSubjectGoal());
-//        lecture.setSubjectClass(lectureDto.getSubjectClass());
-//        lecture.setSubjectDay(lectureDto.getSubjectDay());
-//        lecture.setSubjectPeriod(lectureDto.getSubjectPeriod());
-//        lecture.setSubjectBook(lectureDto.getSubjectBook());
-//        lecture.setSubjectReference(lectureDto.getSubjectReference());
-//        lecture.setSubjectReqStart(lectureDto.getSubjectReqStart());
-//        lecture.setSubjectReqEnd(lectureDto.getSubjectReqEnd());
-//        lecture.setSubjectOpStart(lectureDto.getSubjectOpStart());
-//        lecture.setSubjectOpEnd(lectureDto.getSubjectOpEnd());
-//        lecture.setTestMidterm(lectureDto.getTestMidterm());
-//        lecture.setTestFinal(lectureDto.getTestFinal());
-//        lecture.setTestProject(lectureDto.getTestProject());
-//        lecture.setTestTeamPj(lectureDto.getTestTeamPj());
-//        lecture.setTestAttend(lectureDto.getTestAttend());
-//
-//        System.out.println(lecture.toString());
-//        return lecture;
-//    }
-//
-//
-//
-//    // 엔티티를 디티오로 바꿔서 클라이언트에 뿌리기 위한 메서드
-//    private LectureDto joinLectureAndUser(Lecture lecture){
-//
-//        LectureDto dto = new LectureDto();
-//            dto.setSubjectName(lecture.getSubjectName());
-//            dto.setSubjectScore(lecture.getSubjectScore());
-//            dto.setSubjectTime(lecture.getSubjectTime());
-//            dto.setSubjectClassification(lecture.getSubjectClassification());
-//            dto.setSubjectGrade(lecture.getSubjectGrade());
-//            dto.setSubjectYear(lecture.getSubjectYear());
-//    //        lecture.setSubjectSemester(lectureDto.getSubjectSemester());
-//            dto.setSubjectOutline(lecture.getSubjectOutline());
-//            dto.setSubjectGoal(lecture.getSubjectGoal());
-//            dto.setSubjectClass(lecture.getSubjectClass());
-//            dto.setSubjectDay(lecture.getSubjectDay());
-//            dto.setSubjectPeriod(lecture.getSubjectPeriod());
-//            dto.setSubjectBook(lecture.getSubjectBook());
-//            dto.setSubjectReference(lecture.getSubjectReference());
-//            dto.setSubjectReqStart(lecture.getSubjectReqStart());
-//            dto.setSubjectReqEnd(lecture.getSubjectReqEnd());
-//            dto.setSubjectOpStart(lecture.getSubjectOpStart());
-//            dto.setSubjectOpEnd(lecture.getSubjectOpEnd());
-//            dto.setTestMidterm(lecture.getTestMidterm());
-//            dto.setTestFinal(lecture.getTestFinal());
-//            dto.setTestProject(lecture.getTestProject());
-//            dto.setTestTeamPj(lecture.getTestTeamPj());
-//            dto.setTestAttend(lecture.getTestAttend());
-//            dto.setUserdataNum(lecture.getUser().getUserdataNum());
-//            dto.setUserMajor(lecture.getUser().getUserMajor());
-//            dto.setUserdataName(lecture.getUser().getUserdataName());
-//            dto.setUserPhone(lecture.getUser().getUserPhone());
-//
-//
-//
-//            System.out.println(dto.toString());
-//            return dto;
-//
-//    }
-//
-//    // DB에 저장 시 DropDown 박스 셀렉트 된 부분이 자꾸 콤마가 들어가서 수정하기 위한 메서드
-//    public Lecture saveLecture(Lecture lecture) {
-//
-//        // 강의 등록 시 셀렉트 박스 value 값에 콤마가 들어가서 콤마 삭제하기 위해 추가함.
-//        lecture.setSubjectScore(lecture.getSubjectScore() != null ? lecture.getSubjectScore().replace(",","") : null);
-//        lecture.setSubjectTime(lecture.getSubjectTime() != null ? lecture.getSubjectTime().replace(",","") : null);
-//        lecture.setSubjectClassification(lecture.getSubjectClassification() != null ? lecture.getSubjectClassification().replace(",","") : null);
-//        lecture.setSubjectGrade(lecture.getSubjectGrade() != null ? lecture.getSubjectGrade().replace(",","") : null);
-//        lecture.setSubjectDay(lecture.getSubjectDay() != null ? lecture.getSubjectDay().replace(",","") : null);
-//
-//        System.out.println(lecture.toString());
-//        return lectureRepository.save(lecture);
-//
-//    }
-//
-//}
+package com.example.school_lms.Service;
+
+import com.example.school_lms.dto.LectureDto;
+import com.example.school_lms.entity.Lecture;
+import com.example.school_lms.entity.User;
+import com.example.school_lms.entity.Userdata;
+import com.example.school_lms.repository.LectureRepository;
+import com.example.school_lms.repository.UserRepository;
+import com.example.school_lms.repository.UserdataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class LectureService {
+
+    @Autowired
+    LectureRepository lectureRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    static UserService userService;
+
+    // 강의 등록 메서드. 등록하다 오류 발생 시 롤백하기 위하여 Transactional 어노테이션 사용.
+    @Transactional
+    public static Lecture createLecture(LectureDto lectureDto) {
+
+        Lecture lecture = new Lecture();
+
+        lecture.setSubjectName(lectureDto.getSubjectName());
+        lecture.setSubjectScore(lectureDto.getSubjectScore());
+        lecture.setSubjectTime(lectureDto.getSubjectTime());
+        lecture.setSubjectClassification(lectureDto.getSubjectClassification());
+        lecture.setSubjectGrade(lectureDto.getSubjectGrade());
+        lecture.setSubjectYear(lectureDto.getSubjectYear());
+        lecture.setSubjectSemester(lectureDto.getSubjectSemester());
+        lecture.setSubjectOutline(lectureDto.getSubjectOutline());
+        lecture.setSubjectGoal(lectureDto.getSubjectGoal());
+        lecture.setSubjectClass(lectureDto.getSubjectClass());
+        lecture.setSubjectDay(lectureDto.getSubjectDay());
+        lecture.setSubjectPeriod(lectureDto.getSubjectPeriod());
+        lecture.setSubjectBook(lectureDto.getSubjectBook());
+        lecture.setSubjectReference(lectureDto.getSubjectReference());
+        lecture.setSubjectReqStart(lectureDto.getSubjectReqStart());
+        lecture.setSubjectReqEnd(lectureDto.getSubjectReqEnd());
+        lecture.setSubjectOpStart(lectureDto.getSubjectOpStart());
+        lecture.setSubjectOpEnd(lectureDto.getSubjectOpEnd());
+        lecture.setTestMidterm(lectureDto.getTestMidterm());
+        lecture.setTestFinal(lectureDto.getTestFinal());
+        lecture.setTestProject(lectureDto.getTestProject());
+        lecture.setTestTeamPj(lectureDto.getTestTeamPj());
+        lecture.setTestAttend(lectureDto.getTestAttend());
+
+
+        System.out.println(lecture.toString());
+        return lecture;
+    }
+
+
+
+
+    // DB에 저장 시 DropDown 박스 셀렉트 된 부분이 자꾸 콤마가 들어가서 수정하기 위한 메서드
+    public Lecture saveLecture(Lecture lecture) {
+
+        // 강의 등록 시 셀렉트 박스 value 값에 콤마가 들어가서 콤마 삭제하기 위해 추가함.
+        lecture.setSubjectScore(lecture.getSubjectScore() != null ? lecture.getSubjectScore().replace(",","") : null);
+        lecture.setSubjectTime(lecture.getSubjectTime() != null ? lecture.getSubjectTime().replace(",","") : null);
+        lecture.setSubjectClassification(lecture.getSubjectClassification() != null ? lecture.getSubjectClassification().replace(",","") : null);
+        lecture.setSubjectGrade(lecture.getSubjectGrade() != null ? lecture.getSubjectGrade().replace(",","") : null);
+        lecture.setSubjectDay(lecture.getSubjectDay() != null ? lecture.getSubjectDay().replace(",","") : null);
+        lecture.setSubjectSemester(lecture.getSubjectSemester() != null ? lecture.getSubjectSemester().replace(",","") : null);
+        System.out.println(lecture.toString());
+        return lectureRepository.save(lecture);
+
+    }
+
+}
--- src/main/java/com/example/school_lms/controller/LectureController.java
+++ src/main/java/com/example/school_lms/controller/LectureController.java
@@ -1,6 +1,6 @@
 package com.example.school_lms.controller;
 
-//import com.example.school_lms.Service.LectureService;
+import com.example.school_lms.Service.LectureService;
 import com.example.school_lms.Service.UserService;
 import com.example.school_lms.dto.LectureDto;
 import com.example.school_lms.entity.Lecture;
@@ -13,29 +13,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @Controller
-@RequestMapping(value = "/lecture")
+@RequestMapping(value = "/visang_university" )
 public class LectureController {
 
-//    @Autowired
-//    LectureService lectureService;
-
     @Autowired
-    UserRepository userRepository;
+    LectureService lectureService;
 
     @Autowired
     UserService userService;
 
-    @GetMapping(value = "/regist")
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/lecture/regist")
     public String lecForm(@SessionAttribute(name = "userId", required = false) Long userId, Model model){
 
         model.addAttribute ("loginType", "visang_university");
-        model.addAttribute ("lecDto",new LectureDto());
+
 
         if (userId != null) {
             User user = userService.getLoginUserById(userId);
@@ -43,32 +38,66 @@ public class LectureController {
                 model.addAttribute("user", user);
                 model.addAttribute("userdataName", user.getUserdataName());
 
-                return "lecture/regist";
+                LectureDto lectureDto = new LectureDto();
+                model.addAttribute("lectureDto", lectureDto);
+
+                return "/lecture_regist";
             }
         }
         // userId가 없거나 사용자를 찾을 수 없는 경우 로그인 페이지로 리다이렉트
         return "redirect:/visang_university/login";
     }
 
-//    @GetMapping(value = "/regist")
-//    public String lecForm(Model model){
-//
-//
-//        User user = userRepository.findByUserdataNum(userdataNum).orElse(null);
-//         model.addAttribute("userInfo", );
-//
-//        return "/lecture/regist";
-//    }
 
-    //수강신청 맵핑
-//    @PostMapping(value = "/regist")
-//    public String lecRegist(LectureDto lectureDto){
-//
-//        Lecture lecture = LectureService.createLecture(lectureDto);
-//        lectureService.saveLecture(lecture);
-//
-//        return "lecture/regist";
-//    }
+    //강의등록 맵핑
+    @PostMapping(value = "/lecture/regist")
+    public String lecRegist(LectureDto lectureDto, @SessionAttribute (name="userId") Long userId, Model model){
+
+        //User 정보 가져오기
+        User user = userService.getLoginUserById(userId);
+
+        //Lecture 객체 생성 및 정보 설정
+        Lecture lecture = LectureService.createLecture(lectureDto);
+
+        int sumPercent = lectureDto.getTestMidterm()
+                + lectureDto.getTestFinal()
+                + lectureDto.getTestProject()
+                + lectureDto.getTestTeamPj()
+                + lectureDto.getTestAttend();
+
+        // 100넘겨서 경고말이 하단에 빨간색으로 뜨더라도 드롭다운 박스 빼고 나머지는 입력하던 값 그대로 나옴.
+        if(sumPercent > 100) {
+            model.addAttribute("showWarning", true);
+            model.addAttribute("warningMessage", "평가 비율의 총 합은 100이 되어야 합니다.");
+            model.addAttribute("lectureDto", lectureDto);
+            model.addAttribute("user", user);  // Add user for rendering on the same page
+            model.addAttribute("userdataName", user.getUserdataName());
+            model.addAttribute("loginType", "visang_university");
+
+            return "lecture_regist";
+        }
+
+        // 100넘겨서 경고말이 하단에 빨간색으로 뜨더라도 드롭다운 박스 빼고 나머지는 입력하던 값 그대로 나옴.
+        if(sumPercent < 100) {
+            model.addAttribute("showWarning", true);
+            model.addAttribute("warningMessage", "평가 비율의 총 합은 100이 되어야 합니다.");
+            model.addAttribute("lectureDto", lectureDto);
+            model.addAttribute("user", user);  // Add user for rendering on the same page
+            model.addAttribute("userdataName", user.getUserdataName());
+            model.addAttribute("loginType", "visang_university");
+
+            return "lecture_regist";
+        }
+
+
+        // User 정보 설정 // Join 해놔서 User세팅을 할 수 있음
+        lecture.setUserId(user);
+
+        // Lecture 저장
+        lectureService.saveLecture(lecture);
+
+        return "redirect:/visang_university/lecture/regist";
+    }
 
 
 
